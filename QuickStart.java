a09d0fe7d6e2a79da5af9d10185c7e665ea9c859
Yuta Hoshino <y.hoshino25@gmail.com>
2021-03-29 02:25:51 +0900

    test

--- QuickStart.java
+++ QuickStart.java
@@ -5,5 +5,9 @@ class QuickStart {
       System.out.println("Good evening.");
       System.out.println("Good afternoon.");
       System.out.println("Good Morning.");
+
+     // 改行なし
+      System.out.print("Good Morning.");
+      System.out.print("Good afternoon.");
   }
 }
