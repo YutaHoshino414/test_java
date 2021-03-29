118aa1cea9d105888ab538b5af6c45781fea5ce4
Yuta Hoshino <y.hoshino25@gmail.com>
2021-03-30 00:24:05 +0900

    test

--- sample.java
+++ sample.java
@@ -6,5 +6,9 @@ class sample {
 
       System.out.println("変数numの値は" + num + "です");
 
+      num = 5;
+
+      System.out.println("変数numの値を更新しました");
+      System.out.println("変数numの値は" + num + "です");
   }
 }
