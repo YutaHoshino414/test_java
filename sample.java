028573e3dd009d0a66f47c2e5827bce01519367e
Yuta Hoshino <y.hoshino25@gmail.com>
2021-04-02 23:17:23 +0900

    test

--- sample.java
+++ sample.java
@@ -1,13 +1,10 @@
 class sample {
   public static void main(String[] args) {
       int num;
-
       num = 3;
-
       System.out.println("変数numの値は" + num + "です");
 
       num = 5;
-
       System.out.println("変数numの値を更新しました");
       System.out.println("変数numの値は" + num + "です");
       System.out.println("-----------------------------");
@@ -16,10 +13,14 @@ class sample {
       final double tax = 1.08;
       int fax = 5;
       System.out.println("5万円から4万円に値下げします");
-      tax = 4;
+      // tax = 4;
       System.out.println("Faxの新価格(税込み)");
       System.out.println(fax * tax + "万円");
+      System.out.println("-----------------------------");
 
-
+      int a;
+      a = 100;
+      a++;
+      System.out.println(a);
   }
 }
