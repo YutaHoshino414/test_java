385a751778a166a05bf0b1657b0d4f4ffaf755d9
Yuta Hoshino <y.hoshino25@gmail.com>
2021-04-07 00:28:58 +0900

    updated

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class sum {
+  public static void main(String[] args){
+
+    int a[] = {1,3,10,2,8};
+
+    int sum = 0;
+
+    for(int i=0; i<a.length;i++){
+      sum += a[i];
+    }
+
+    System.out.println("SUM＝" + sum);
+  }
+}
